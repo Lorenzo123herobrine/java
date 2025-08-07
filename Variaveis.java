8191deafc3e6480ab10ca7ccfe198636b47e80e9
Lorenzo123herobrine <159460377+Lorenzo123herobrine@users.noreply.github.com>
2025-08-07 09:55:17 -0300

    Add files via upload

---
+++ Variaveis.java
@@ -0,0 +1,14 @@
+public class Variaveis{
+    public static void main (String[] args){
+        //tipo nome = conteudo;
+        String meuNome = "Lorenzo";
+        meuNome += " Justulim";
+        int idade = 18;
+        double altura = 1.82;
+        char sexo = 'M';
+        boolean casado = true;
+
+        System.out.println ("Meu nome é "+meuNome+" minha idade é "+idade+" anos e tenho "+altura+" de altura");
+        System.out.print ("Sou casado? "+casado);
+    }
+}
